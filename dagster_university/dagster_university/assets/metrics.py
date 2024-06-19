f9975f20e6b4d5d336d4c1145e16507199914912
shrish <adhikari.shrish1@gmail.com>
2024-06-19 11:46:48 +0545

    Added manhattan stats

--- dagster_university/dagster_university/assets/metrics.py
+++ dagster_university/dagster_university/assets/metrics.py
@@ -12,4 +12,23 @@ from . import constants
 
 @asset(deps=["taxi_trips", "taxi_zones"])
 def manhattan_stats() -> None:
-    pass
+    query = """
+        SELECT
+            zones.zone,
+            zones.borough,
+            zones.geometry,
+            count(1) as num_trips,
+        FROM trips
+        LEFT JOIN zones ON trips.pickup_zone_id = zones.zone_id
+        WHERE borough = 'Manhattan' AND geometry IS NOT NULL
+        GROUP BY zone, borough, geometry
+        """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    trips_by_zone = conn.execute(query).fetch_df()
+
+    trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
+    trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
+
+    with open(constants.MANHATTAN_STATS_FILE_PATH, "w") as output_file:
+        output_file.write(trips_by_zone.to_json())
