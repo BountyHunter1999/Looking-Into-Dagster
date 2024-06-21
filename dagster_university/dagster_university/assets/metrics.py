bfd2e3da75db7c9f18c6fa4df82ecf45877b002c
shrish <adhikari.shrish1@gmail.com>
2024-06-21 07:16:00 +0545

    Added trips by week

--- dagster_university/dagster_university/assets/metrics.py
+++ dagster_university/dagster_university/assets/metrics.py
@@ -1,8 +1,10 @@
 from dagster import asset
 
+from datetime import datetime, timedelta
 import plotly.express as px
 import plotly.io as pio
 import geopandas as gpd
+import pandas as pd
 
 import duckdb
 import os
@@ -40,16 +42,76 @@ def manhattan_stats() -> None:
 def manhattan_map() -> None:
     trips_by_zone = gpd.read_file(constants.MANHATTAN_STATS_FILE_PATH)
 
-    fig = px.choropleth_mapbox(trips_by_zone,
+    print(trips_by_zone.head())
+    print(trips_by_zone.geometry.__geo_interface__)
+
+    fig = px.choropleth_mapbox(
+        trips_by_zone,
         geojson=trips_by_zone.geometry.__geo_interface__,
         locations=trips_by_zone.index,
-        color='num_trips',
-        color_continuous_scale='Plasma',
-        mapbox_style='carto-positron',
-        center={'lat': 40.758, 'lon': -73.985},
+        color="num_trips",
+        color_continuous_scale="Plasma",
+        mapbox_style="carto-positron",
+        center={"lat": 40.758, "lon": -73.985},
         zoom=11,
         opacity=0.7,
-        labels={'num_trips': 'Number of Trips'}
+        labels={"num_trips": "Number of Trips"},
     )
 
     pio.write_image(fig, constants.MANHATTAN_MAP_FILE_PATH)
+
+
+@asset(
+    deps=["taxi_trips"],
+)
+def trips_by_week() -> None:
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    start_date = datetime.strptime("2023-03-01", constants.DATE_FORMAT)
+    end_date = datetime.strptime("2023-04-01", constants.DATE_FORMAT)
+
+    result = pd.DataFrame()
+
+    while start_date < end_date:
+        start_date_str = start_date.strftime(constants.DATE_FORMAT)
+        query = f"""
+            SELECT 
+                vendor_id, total_amount, trip_distance, passenger_count
+            FROM trips
+            WHERE DATE_TRUNC('week', pickup_datetime) = DATE_TRUNC('week', '{start_date_str}'::date)
+        """
+
+        data_for_week = conn.execute(query).fetch_df().head()
+
+        print(data_for_week)
+        aggregate = (
+            data_for_week.agg(
+                {
+                    "vendor_id": "count",
+                    "total_amount": "sum",
+                    "trip_distance": "sum",
+                    "passenger_count": "sum",
+                }
+            )
+            .rename({"vendor_id": "num_trips"})
+            .to_frame()
+            .T
+        )
+
+        aggregate["period"] = start_date
+
+        result = pd.concat([result, aggregate])
+        start_date += timedelta(days=7)
+
+    # clean up the formatting of the dataframe
+    result["passenger_count"] = result["passenger_count"].astype(int)
+    result["num_trips"] = result["num_trips"].astype(int)
+    result["total_amount"] = result["total_amount"].round(2).astype(float)
+    result["trip_distance"] = result["trip_distance"].round(2).astype(float)
+    result = result[
+        ["period", "num_trips", "total_amount", "trip_distance", "passenger_count"]
+    ]
+    result = result.sort_values(by="period")
+
+    print(result.head())
+
+    result.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
